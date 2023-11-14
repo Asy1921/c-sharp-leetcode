17abecf58bf527782423f13d035b6e3cfda96a1b
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2023-11-14 21:17:21 +0530

    Longest Increasing Subsequence

---
+++ LongestIncreasingSubsequence.cs
@@ -0,0 +1,37 @@
+// 300.Longest Increasing Subsequence
+// Medium
+// 19.2K
+// 361
+// Companies
+// Given an integer array nums, return the length of the longest strictly increasing 
+// subsequence
+// .
+
+
+
+namespace LeetCodeProblems;
+public partial class LCProblems
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int[] dp = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            dp[i] = 1;
+        }
+        for (int i = nums.Length - 2; i >= 0; i--)
+
+        {
+            for (int j = i; j < nums.Length; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+
+                    dp[i] = Math.Max(dp[i], 1 + dp[j]);
+                }
+
+            }
+        }
+        return dp.Max();
+    }
+}
