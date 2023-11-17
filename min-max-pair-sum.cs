f43d082da398e8532c20d4fdd4c921eeba7138a3
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2023-11-17 16:25:21 +0530

    Create min-max-pair-sum.cs

---
+++ min-max-pair-sum.cs
@@ -0,0 +1,26 @@
+//1877. Minimize Maximum Pair Sum in Array
+//Medium
+//1.6K
+//347
+//Companies
+//The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+//For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+//Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+//Each element of nums is in exactly one pair, and
+//The maximum pair sum is minimized.
+//Return the minimized maximum pair sum after optimally pairing up the elements.
+namespace LeetCodeProblems;
+public partial class LCProblems{
+
+    public int MinPairSum(int[] nums) {
+        Array.Sort(nums);
+        int res=-1;
+        for(int i=0;i<nums.Length/2;i++)
+        {
+            res=Math.Max(res,nums[i]+nums[nums.Length-1-i]);
+        }
+        return res;
+    }
+}
