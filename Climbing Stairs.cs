f7b2bf94ab41da67fca2ebd270b8c2b2ac32ae17
Asy1921 <amitabh.suyash@gmail.com>
2024-01-19 00:37:07 +0530

    Bracket Issue Fixed

--- Climbing Stairs.cs
+++ Climbing Stairs.cs
@@ -16,15 +16,17 @@
 namespace LeetCodeProblems;
 public partial class LCProblems
 {
-    int[] dp = new int[n + 1];
-    dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
+    public int ClimbingStairs()
+    {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
         {
-            dp[i]=0;
-            for(int j=1;j<=2&&j<=i;j++)
+            dp[i] = 0;
+            for (int j = 1; j <= 2 && j <= i; j++)
             {
-                dp[i]+=dp[i - j];
+                dp[i] += dp[i - j];
             }
         }
         return dp[n];
